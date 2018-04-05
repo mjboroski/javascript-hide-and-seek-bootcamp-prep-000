39b9d33fe56a426bb03e895c53e2adedb4261628
Michael Boroski <mjboroski@gmail.com>
2018-04-05 15:19:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  var selection=document.getElementById('div#grand-node');
+  var selection=document.getElementById('grand-node');
   for (let i = 0; i < selection.length,i>0; i++) {
     if(selection[i].div!=undefined){
       let selection = selection[i].div
